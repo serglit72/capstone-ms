267fa2c926aabc99cd8e550a678838de0fad780b
Serglit <serglit72@gmail.com>
2022-12-02 11:45:46 -0800

    echo "Privet"

--- eks/eks-template-update.sh
+++ eks/eks-template-update.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-
+echo "Privet"
+sed --help
 # sample value for your variables
-MYVARVALUE="nginx:latest"
+# MYVARVALUE="nginx:latest"
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-template = cat eks-cluster-spot-copy.yaml | sed -i 's/{{PRIVATE_SUB_01}}/$PRIVATE_SUB_01/g'
-template = cat eks-cluster-spot-copy.yaml | sed -i 's/{{PRIVATE_SUB_01}}/$PRIVATE_SUB_02/g'
-template = cat eks-cluster-spot-copy.yaml | sed -i 's/{{PUBLIC_SUB_01}}/$PUBLIC_SUB_01/g'
-template = cat eks-cluster-spot-copy.yaml | sed -i 's/{{PUBLIC_SUB_02}}/$PUBLIC_SUB_02/g'
+template = cat eks-cluster-spot-copy.yaml | sed -i 's/PRIVATE_SUB_01/${PRIVATE_SUB_01}/g'
+template = cat eks-cluster-spot-copy.yaml | sed -i 's/PRIVATE_SUB_01/${PRIVATE_SUB_02}/g'
+template = cat eks-cluster-spot-copy.yaml | sed -i 's/PUBLIC_SUB_01/$PUBLIC_SUB_01/g'
+template = cat eks-cluster-spot-copy.yaml | sed -i 's/PUBLIC_SUB_02/$PUBLIC_SUB_02/g'
 #sed "s/{{MYVARNAME}}/$MYVARVALUE/g"`
 ls -la
-cat eks-cluster-spot-copy.yaml
+cat "eks-cluster-spot-copy.yaml"
 pwd
-echo $template
+echo "$template"
 
 # apply the yml with the substituted value
 # echo "$template" | kubectl apply -f -
