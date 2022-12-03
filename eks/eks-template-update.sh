22e18b872cf1f52a6b0e6c53fb7e1bdc7a85935d
Serglit <serglit72@gmail.com>
2022-12-02 21:08:12 -0800

    fix

--- eks/eks-template-update.sh
+++ eks/eks-template-update.sh
@@ -7,8 +7,10 @@ PRIVATE_SUB_01=$(aws cloudformation describe-stack-resources --stack-name eks-vp
 PRIVATE_SUB_02=$(aws cloudformation describe-stack-resources --stack-name eks-vpc-9b633a6 --query 'StackResources[?LogicalResourceId==`PrivateSubnet02`].PhysicalResourceId' --output=text)
 PUBLIC_SUB_01=$(aws cloudformation describe-stack-resources --stack-name eks-vpc-9b633a6 --query 'StackResources[?LogicalResourceId==`PublicSubnet01`].PhysicalResourceId' --output=text)
 PUBLIC_SUB_02=$(aws cloudformation describe-stack-resources --stack-name eks-vpc-9b633a6 --query 'StackResources[?LogicalResourceId==`PublicSubnet02`].PhysicalResourceId' --output=text)
-template=cat "eks-cluster-spot-copy.yaml" | i=1 while [ $i -le 4 ];\
-do sed "s/PRIVATE_SUB_01/$PRIVATE_SUB_01/g" \
+template=cat "eks-cluster-spot-copy.yaml" | \
+i=1 while [ $i -le 4 ]
+do 
+    sed "s/PRIVATE_SUB_01/$PRIVATE_SUB_01/g" \
     sed "s/PRIVATE_SUB_02/$PRIVATE_SUB_02/g" \
     sed "s/PUBLIC_SUB_01/$PUBLIC_SUB_01/g" \
     sed "s/PUBLIC_SUB_02/$PUBLIC_SUB_02/g" \
