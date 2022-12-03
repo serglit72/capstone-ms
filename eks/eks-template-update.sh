759cd70cc1ea6335fd07e83e63b7397a03c10d2a
Serglit <serglit72@gmail.com>
2022-12-02 18:33:47 -0800

    template= cat "$template"

--- eks/eks-template-update.sh
+++ eks/eks-template-update.sh
@@ -8,9 +8,9 @@ PRIVATE_SUB_02=$(aws cloudformation describe-stack-resources --stack-name eks-vp
 PUBLIC_SUB_01=$(aws cloudformation describe-stack-resources --stack-name eks-vpc-9b633a6 --query 'StackResources[?LogicalResourceId==`PublicSubnet01`].PhysicalResourceId' --output=text)
 PUBLIC_SUB_02=$(aws cloudformation describe-stack-resources --stack-name eks-vpc-9b633a6 --query 'StackResources[?LogicalResourceId==`PublicSubnet02`].PhysicalResourceId' --output=text)
 template= cat "eks-cluster-spot-copy.yaml" | sed "s/PRIVATE_SUB_01/$PRIVATE_SUB_01/g"
-template= cat $template | sed "s/PRIVATE_SUB_02/$PRIVATE_SUB_02/g"
-template= cat $template | sed "s/PUBLIC_SUB_01/$PUBLIC_SUB_01/g"
-template= cat $template | sed "s/PUBLIC_SUB_02/$PUBLIC_SUB_02/g"
+template= cat "$template" | sed "s/PRIVATE_SUB_02/$PRIVATE_SUB_02/g"
+template= cat "$template" | sed "s/PUBLIC_SUB_01/$PUBLIC_SUB_01/g"
+template= cat "$template" | sed "s/PUBLIC_SUB_02/$PUBLIC_SUB_02/g"
 #sed "s/{{MYVARNAME}}/$MYVARVALUE/g"
 # apply the yml with the substituted value
 echo "$template" #| kubectl apply -f -
