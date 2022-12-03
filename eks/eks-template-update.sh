f2a6e3e8113376b040abec65070c98b5bd086f2d
Serglit <serglit72@gmail.com>
2022-12-02 17:39:24 -0800

    cat eks-cluster-spot-copy.yaml

--- eks/eks-template-update.sh
+++ eks/eks-template-update.sh
@@ -13,15 +13,15 @@
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-echo "I'm in (pwd)" 
+echo "I'm in "$pwd 
 
 # new_file=cat "eks-cluster-spot-copy.yaml" | \
 str='PRIVATE_SUB_01/$PRIVATE_SUB_01 PRIVATE_SUB_02/$PRIVATE_SUB_02 \
 PUBLIC_SUB_01/$PUBLIC_SUB_01 PUBLIC_SUB_02/$PUBLIC_SUB_02'; \
-for s in $str do sed "s/$s/g" eks-cluster-spot-copy.yaml done;
+for s in $str do sed "s/$s/g" eks-cluster-spot-copy.yaml done
 # cat "eks-cluster-spot-copy.yaml" | sed "s/PRIVATE_SUB_01/$PRIVATE_SUB_02/g"
 # cat "eks-cluster-spot-copy.yaml" | sed "s/PUBLIC_SUB_01/$PUBLIC_SUB_01/g"
 # cat "eks-cluster-spot-copy.yaml" | sed "s/PUBLIC_SUB_02/$PUBLIC_SUB_02/g"
-#sed "s/{{MYVARNAME}}/$MYVARVALUE/g"`
+#sed "s/{{MYVARNAME}}/$MYVARVALUE/g"
 # apply the yml with the substituted value
 # echo "$template" | kubectl apply -f -
